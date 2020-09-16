5d632a8c41924d2a47809db8da7a06a4a2f21569
David Alzate <illker@hotmail.com>
2020-09-16 19:40:55 +0000

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-char string[]="\"Programming is like building a multilingual puzzle"
+char string[]="\"Programming is like building a multilingual puzzle";
 puts(string);
 return (0);
 }
