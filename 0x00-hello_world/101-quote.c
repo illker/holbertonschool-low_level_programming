2516111bd6d9ae9d56cda3ad8970620efdbf83ee
David Alzate <illker@hotmail.com>
2020-09-16 21:33:18 +0000

    0x00. C - Hello, World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+/* understand the simplicity */
+
+#include <stdio.h>
+
+/**
+* main - write
+* Description: write instead printf
+* Return: 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
