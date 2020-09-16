5b85f550d6d435cb9e69e8b5734788f05ac68391
David Alzate <illker@hotmail.com>
2020-09-16 20:08:58 +0000

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/* Hello, printf */
+
+#include <stdio.h>
+
+/**
+* main - print
+* Description: uses print to output message
+* Return: 0
+*/
+
+int main(void)
+{
+char string[] = "with proper grammar, but the outcome is a piece of art";
+printf(string);
+return (0);
+}
+
