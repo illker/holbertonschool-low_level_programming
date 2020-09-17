d9151b6ce96b50f4874d53b101f557d49653c0f0
David Alzate <illker@hotmail.com>
2020-09-17 21:07:56 +0000

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (mod == 0)
 {
 printf("%s %d is %d and is 0\n", ldo, n, mod);
 }
-else if ((mod > 6) && (mod != 0))
+else if (mod < 6 && mod != 0)
 {
 printf("%s %d is %d and is less than 6 and not 0\n", ldo, n, mod);
 }
