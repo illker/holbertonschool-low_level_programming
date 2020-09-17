29899e12c2e9cd0dac40310d31e2982df80e2aa4
David Alzate <illker@hotmail.com>
2020-09-17 21:42:50 +0000

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,8 +13,10 @@ int main(void)
 {
 char ap;
 
-for (ap = 'a' ; ap <= 'z' ; ap++) putchar (ap);
-for (ap = 'A' ; ap <= 'Z' ; ap++) putchar (ap);
+for (ap = 'a' ; ap <= 'z' ; ap++)
+    putchar (ap);
+for (ap = 'A' ; ap <= 'Z' ; ap++)
+    putchar (ap);
 putchar('\n');
 
 return (0);
