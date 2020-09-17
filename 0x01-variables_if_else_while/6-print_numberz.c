fb0969fc530075ee2884f1abe40a362a7fa918be
David Alzate <illker@hotmail.com>
2020-09-17 23:50:27 +0000

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+/* Numberz */
+
+#include <stdio.h>
+
+/**
+* main - Numbers
+* Description: prints all numbers of base 10 starting from 0 ASCII
+* Return: 0
+*/
+
+int main(void)
+{
+int n;
+
+n = 48;
+
+while (n <= 57)
+{
+putchar (n);
+n++;
+}
+
+printf("\n");
+
+return (0);
+}
