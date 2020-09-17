5991a872a774aade5d194d41dbaa99055323ce86
David Alzate <illker@hotmail.com>
2020-09-17 21:40:49 +0000

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+/* alphABET */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - alphABET
+* Description:  alphabet in lowercase, and then in uppercase
+* Return: 0
+*/
+
+int main(void)
+{
+char ap;
+
+for (ap = 'a' ; ap <= 'z' ; ap++) putchar (ap);
+for (ap = 'A' ; ap <= 'Z' ; ap++) putchar (ap);
+putchar('\n');
+
+return (0);
+}
