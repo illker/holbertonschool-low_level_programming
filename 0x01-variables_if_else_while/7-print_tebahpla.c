4a2aca204fe5d87c5a2a3224258c6fbf277b001c
David Alzate <illker@hotmail.com>
2020-09-18 01:33:53 +0000

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+/* Smile in the mirror */
+
+#include <stdio.h>
+
+/**
+* main - Mirror
+* Description: lowercase alphabet in reverse
+* Return: 0
+*/
+
+int main(void)
+{
+char a;
+
+for (a = 'z' ; a >= 'a' ; a--)
+putchar (a);
+
+putchar('\n');
+
+return (0);
+}
