fd4b989bd7d0e255d0435557cb9040f27c765077
David Alzate <illker@hotmail.com>
2020-09-18 02:32:40 +0000

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+/*  Patience */
+
+#include <stdio.h>
+
+/**
+* main - Patience
+* Description: possible combinations
+* Return: 0
+*/
+
+int main(void)
+{
+int n, m;
+
+n = 48;
+
+while (n <= 57)
+{
+putchar (n);
+putchar (44);
+putchar (127);
+n++;
+}
+
+putchar(10);
+
+return (0);
+}
