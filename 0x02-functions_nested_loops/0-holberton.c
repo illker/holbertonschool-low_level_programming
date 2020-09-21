adda8164f82b3a6d006fa74804628a655a30ef74
David Alzate <illker@hotmail.com>
2020-09-21 21:46:30 +0000

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,27 @@
+/* prints Holberton */
+
+#include <holberton.h>
+
+/**
+* main -  prints Holberton holberton.h
+* Description: custom holberton.h
+* Return: 0
+*/
+
+int main(void)
+{
+
+_putchar(H);
+_putchar(o);
+_putchar(l)
+_putchar(b)
+_putchar(e)
+_putchar(r)
+_putchar(t)
+_putchar(o)
+_putchar(n)
+_putchar(10)
+
+return (0);
+
+}
