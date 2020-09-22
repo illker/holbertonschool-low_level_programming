33eadff665301afdd7ec63a77fc28800bb65209d
David Alzate <illker@hotmail.com>
2020-09-22 22:58:27 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,4 @@
-/* 4-isalpha.c */
-
-#include <holberton.h>
+#include "holberton.h"
 
 /**
 * _isalpha -  Prototype
@@ -8,14 +6,14 @@
 * Return: 0
 */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
 return (1);
+}
 else
 {
 return (0);
-}
 }
 }
