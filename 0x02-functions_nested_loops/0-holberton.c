4da6c7e7fac5c41d5529fffcdf6a61361cf6398d
David Alzate <illker@hotmail.com>
2020-09-22 20:11:49 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -11,14 +11,14 @@ int main(void)
 
 _putchar(48);
 _putchar('o');
-_putchar(108)
-_putchar('b')
-_putchar(101)
-_putchar('r')
-_putchar(116)
-_putchar('o')
-_putchar(110)
-_putchar(10)
+_putchar(108);
+_putchar('b');
+_putchar(101);
+_putchar('r');
+_putchar(116);
+_putchar('o');
+_putchar(110);
+_putchar(10);
 
 return (0);
 
