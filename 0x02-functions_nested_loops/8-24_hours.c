229a7a2721cc040efe182962dfe3c604fd4a0f26
David Alzate <illker@hotmail.com>
2020-09-23 02:28:34 +0000

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,4 @@
-/* 8-24_hours.c */
-
-#include <holberton.h>
+#include "holberton.h"
 
 /**
 * jack_bauer -  Prototype
@@ -16,12 +14,12 @@ for (n = 0; n < 24 ; n++)
 {
 for (m = 0; m < 60 ; m++)
 {
-_putchar(n / 10 + 48); 
-_putchar(n % 10 + 48);
+_putchar((n / 10) + 48);
+_putchar((n % 10) + 48);
 _putchar(58);
-_putchar(m / 10 + 48); 
-_putchar(m % 10 + 48);
-putchar(10);
+_putchar((m / 10) + 48);
+_putchar((m % 10) + 48);
+_putchar(10);
 }
 }
 }
