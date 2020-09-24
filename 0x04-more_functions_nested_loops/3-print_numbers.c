928bea54134206678640104dda1d7347ffa182a2
David Alzate <illker@hotmail.com>
2020-09-24 23:53:31 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prototype function
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = 48;
+
+	while (a >= 57)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar(10);
+}
