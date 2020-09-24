1391a77da184eb720b1076d21d0c402906025313
David Alzate <illker@hotmail.com>
2020-09-24 23:30:05 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - prototype function
+ * @a: variable int c
+ * @b: variable int c
+ * Return: multiplication a * b
+ */
+
+int mul(int a, int b)
+{
+    return (a * b);
+}
