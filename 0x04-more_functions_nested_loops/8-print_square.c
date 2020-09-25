79116dcfe9d55fe3dec69ac04aab6ade968b2f94
David Alzate <illker@hotmail.com>
2020-09-25 04:46:14 +0000

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_square - prototype function
+ * @size: Variable int
+ * Return: n
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar(10);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar(35);
+		_putchar(10);
+	}
+}
