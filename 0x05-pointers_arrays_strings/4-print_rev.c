a13c57ab4bd5466acf3721023cce3cb3cd9d0e7c
David Alzate <illker@hotmail.com>
2020-09-29 16:40:40 +0000

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,8 @@ void print_rev(char *s)
 {
 	int a;
 
-	a = 0;
-	while (s[a] != '\0')
-		a++;
+	a = _strlen(s);
+	a--;
 
 	while (a >= 0)
 	{
@@ -20,4 +19,21 @@ void print_rev(char *s)
 		a--;
 	}
 	_putchar(10);
+}
+
+/**
+ * _strlen - prototype function
+ * @s: - Variable string pointer
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+		a++;
+
+	return (a);
 }
