c7275fbd30c250109b9e87d606c73747b2644dc9
David Alzate <illker@hotmail.com>
2020-09-30 04:40:39 +0000

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - prototype function
+ * @dest: Variable char pointer
+ * @src: Variable char pointer
+ * Return: dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; *(src + a) != '\0'; a++)
+	{
+		*(dest + a) = *(src + a);
+	}
+
+	return (dest);
+}
