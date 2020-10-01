c10070eb7d79f7ded185398adcfc7583642db680
David Alzate <illker@hotmail.com>
2020-10-01 22:44:27 +0000

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - prototype function
+ * @dest: - Variable char pointer
+ * @src: - Variable char pointer
+ * @n: Variable int
+ * Return: char dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (src[a] != '\0' && a < n)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	while (a < n)
+	{
+		dest[a] = '\0'
+		a++;
+	}
+	return (dest);
+}
