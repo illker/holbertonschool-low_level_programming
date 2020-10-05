81c278e71da57224d488aca9fab013fcab19a55b
David Alzate <illker@hotmail.com>
2020-10-05 19:50:01 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * *_memset - prototype function
+ * @s: - Variable char pointer
+ * @b: - Variable char
+ * @n: - Variable unsigned n
+ * Return: int value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	while (n--)
+	{
+		s[n] = b;
+	}
+	return(s);
+}
