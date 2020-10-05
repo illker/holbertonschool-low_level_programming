34b800b25114d2f1f80a94d313d28dceb25de2b9
David Alzate <illker@hotmail.com>
2020-10-05 20:33:36 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - prototype function
+ * @dest: - Variable char pointer
+ * @src: - Variable char pointer
+ * @n: - Variable unsigned n
+ * Return: int value
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < n)
+	{
+		dest[n] = src[n];
+		a++;
+	}
+	return (dest);
+}
