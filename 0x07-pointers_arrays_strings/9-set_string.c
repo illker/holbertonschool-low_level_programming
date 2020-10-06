5ffc2aa4f70d57d11278350f1f328bcb3dbdaf9c
David Alzate <illker@hotmail.com>
2020-10-06 03:48:45 +0000

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - prototype function
+ * @s: - Variable char pointer
+ * @to: - Variable int
+ * Return: Void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
