b56f49d6951da5f4167101a9705d3a39c3eb19fb
David Alzate <illker@hotmail.com>
2020-10-08 05:20:10 +0000

    0x08. C - Recursion

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+ * teamf1 - prototype
+ * @s: String
+ * Return: int
+ */
+
+int teamf1(char *s)
+{
+	int a;
+
+	a = 0;
+	if (*s != 0)
+	{
+		a++;
+		return (a + teamf1(s + 1));
+	}
+	return (a);
+}
+
+/**
+ * teamf2 - prototype
+ * @s: Variable char
+ * @b: Variable int
+ * Return: 1 or 0
+ */
+
+int teamf2(char *s, int b)
+{
+	if (b <= 1)
+		return (1);
+	if (*s != s[b - 1])
+		return (0);
+	else
+		return (teamf2((s + 1), (b - 2)));
+}
+
+/**
+ * is_palindrome - prototype func
+ * @s: String
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	int a;
+
+	a = teamf1(s);
+	if (a <= 1)
+		return (1);
+	return (teamf2(s, i));
+}
