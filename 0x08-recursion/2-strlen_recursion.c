0c819616d204b2f241df50a3a55775f8b3e8eb36
David Alzate <illker@hotmail.com>
2020-10-08 01:33:24 +0000

    0x08. C - Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 
 /**
- * _print_rev_recursion - prototype function
- * @s: - Variable char pointer
- * Return: Void
+ * _strlen_recursion - prototype function
+ * @s: - Variable int pointer
+ * Return: int value
  */
 
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	int a;
+
+	if (*s == '\0')
+		return (0);
+	a = (1 + _strlen_recursion(s + 1));
+	return (a);
 }
