267453872413e4eb3f8cebaaaf2a4e7e2d93632d
David Alzate <illker@hotmail.com>
2020-10-08 03:54:20 +0000

    0x08. C - Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - prototype function
+ * @n: - Variable int
+ * Return: int value
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (teamf(n, 1));
+}
+
+/**
+ * teamf - prototype function
+ * @a: - Variable int
+ * @b: - Variable int
+ * Return: int value
+ */
+
+int teamf(int a, int b)
+{
+	if (b * b < a)
+		return (teamf(n, b + 1));
+	else if (b * b > a)
+		return (-1);
+	else
+		return (b);
+}
