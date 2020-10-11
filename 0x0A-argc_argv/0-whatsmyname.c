4ef7b15aec5515b295b9d5cdafe62247c585fe82
David Alzate <illker@hotmail.com>
2020-10-11 02:49:38 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: - Variable int
+ * @argv: - Variable array
+ * Return: int value
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = 0;
+
+	for (a = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
