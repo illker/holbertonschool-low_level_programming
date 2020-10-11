d2e74295979127203a46a2ce0bcbb94b23ba7761
David Alzate <illker@hotmail.com>
2020-10-11 03:21:36 +0000

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: - Variable int
+ * @argv: - Variable array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
