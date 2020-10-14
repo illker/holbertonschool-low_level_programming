494c105836244ea5a1cede59e6588f59f4e6899b
David Alzate <illker@hotmail.com>
2020-10-14 04:26:40 +0000

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *argstostr - function that creates an array of chars
+ * @ac: - Variable int
+ * @av: - Variable char
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *a;
+	int b, c, d = 0, e;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (b = 0; b < ac; b++)
+	{
+		for (c = 0; av[b][c] != '\0'; c++)
+		{
+			d++;
+		}
+		d++;
+	}
+	a = malloc(d * sizeof(a));
+	if (a == NULL)
+		return (NULL);
+	e = 0;
+	for (b = 0; b < ac; b++)
+	{
+		for (c = 0; av[b][c] != '\0'; c++)
+		{
+			a[e] = av[b][c];
+			e++;
+		}
+		a[e] = '\n';
+		e++;
+	}
+	return (a);
+}
