5cb69e909bdc94f1e2944847ad09863e8aa14ecd
David Alzate <illker@hotmail.com>
2020-10-14 03:40:29 +0000

    0x0B. C - malloc, free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,12 +6,12 @@
  * free_grid - function that creates an array of chars
  * @grid: - Variable int
  * @height: - Variable int
- * Return: pointer to a 2 dimensional array
+ * Return: void
  */
 
 void free_grid(int **grid, int height)
 {
-	int a, b;
+	int a;
 
 	for (a = 0; a < height; a++)
 		free(grid[a]);
