e8adbd2cbe705521ab874c8ca275e16d5bb9540c
David Alzate <illker@hotmail.com>
2020-10-16 01:45:06 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory for an array,
+ * @nmemb: Variable pointer
+ * @size: Variable pointer
+ * Return: a
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *a;
+	unsigned int b;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	a = malloc((nmemb * size) * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
+}
