ef4edb3decc90a6a7f0c70ff96d0f31dda379c0f
David Alzate <illker@hotmail.com>
2020-10-19 22:23:35 +0000

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Data type
+ * @name: Variable char
+ * @age: Variable float
+ * @owner: Variable char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char  *owner;
+};
+
+#endif /* DOG_H */
