c4682bd3c48cd3d3d66bf7dc63a8b559c5c70b37
David Alzate <illker@hotmail.com>
2020-10-19 22:58:01 +0000

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - Prototype
+* @d: Struct dog
+* @name: Variable char
+* @age: Variable float
+* @owner: Variable char
+* Return: Void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	(*d).owner = owner;
+}
