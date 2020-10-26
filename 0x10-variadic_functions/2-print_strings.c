7c1a715712ee628c853613e77762c9a24c988817
David Alzate <illker@hotmail.com>
2020-10-26 02:33:59 +0000

    prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - sum of all parameters
+ * @separator: arguments to function
+ * @n: arguments to function
+ * Return: sum
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list list;
+	char *s = NULL;
+
+	va_start(list, n);
+	
+	for (a = 0; a < n; a++)
+	{
+		s = va_arg(list, char *);
+		if (s == 0)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+}
