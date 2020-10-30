b01067606ad8bcbb57635048917f1512fb7b57a5
David Alzate <illker@hotmail.com>
2020-10-30 21:20:32 +0000

    advanced 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * teamf - print all elements of a list_t list
+ * __attribute__
+ * Return: void
+ */
+
+__attribute__ ((constructor))
+void teamf (void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
