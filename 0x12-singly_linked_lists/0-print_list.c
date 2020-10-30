5b9ecdecbb061559784f526a364c912ed3bdeebd
David Alzate <illker@hotmail.com>
2020-10-30 16:02:18 +0000

    print_list3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,14 @@
 size_t print_list(const list_t *h)
 {
 	int a;
-	if (h == NULL)
-		return (0);
+
 	for (a = 0; h != NULL; a++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
 	}
 	return (a);
 }
