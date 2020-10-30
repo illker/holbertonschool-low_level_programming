5b0d2d53100cc250a9998195558187b1e65aae6e
David Alzate <illker@hotmail.com>
2020-10-30 15:45:25 +0000

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements of a list_t list
+ * @h: linked list to print
+ * Return: message to stdout
+ */
+
+size_t print_list(const list_t *h)
+{
+	int a;
+
+	for (a = 0; h != NULL; a++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (a);
+}
