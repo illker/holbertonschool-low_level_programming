6028d3a5ead62e5fd8d8393d5bbf748f5c3c4885
David Alzate <illker@hotmail.com>
2020-10-30 19:46:54 +0000

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * *add_node_end - print all elements of a list_t list
+ * @head: linked list to print
+ * @str: Variable char
+ * Return: number of elements
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+ {
+	list_t *n;
+	list_t *c;
+	int a;
+
+	n = malloc(sizeof(list_t));
+	if (n == NULL)
+		return (NULL);
+
+	if (*head != NULL)
+	{
+		c = *head;
+		for (a = 0; c->next != NULL; a++)
+		{
+			c = c->next;
+		}
+		c->next = n;
+	}
+	else
+		*head = n;
+
+	n->next = NULL;
+	n->str = strdup(str);
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	n->len = a;
+
+	return (n);
+}
