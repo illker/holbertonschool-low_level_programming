087e247a9f4c27dc822d65639b4a5ee3a397bb20
David Alzate <illker@hotmail.com>
2020-11-05 04:43:42 +0000

    :mag:

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Print a `listint_t`
+ * @head: head of linked list
+ * Return: number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	
+}
