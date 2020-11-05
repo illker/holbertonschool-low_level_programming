de8e1517db2d907b3c2509bc76688b0d52dc0743
David Alzate <illker@hotmail.com>
2020-11-05 04:41:31 +0000

    :mag:

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverses a linked list
+  * @head: pointer to a pointer
+  * Return: head to NULL
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+
+}
