ffdfaae69d7997ebda11e2421b8cde2c76517e72
David Alzate <illker@hotmail.com>
2020-11-05 01:11:02 +0000

    :rewind::rewind:

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  * pop_listint - function that deletes the head node & returns its data
+  * @head: pointer to a pointer
+  * Return: n, head's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *copy;
+
+	copy = malloc(sizeof(listint_t));
+	if (copy == NULL)
+		return (0);
+	if (*head == NULL)
+		return (0);
+	copy = *head;
+	*head = copy->next;
+	n = copy->n;
+	free(copy);
+	return (n);
+}
