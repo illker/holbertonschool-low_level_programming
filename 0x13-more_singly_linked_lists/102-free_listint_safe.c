fef1d1d4d65f936a66f2d057d9f8fa9f2d6eea9c
David Alzate <illker@hotmail.com>
2020-11-05 04:45:15 +0000

    :mag:

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - free a `listint_t`
+ * @h: double pointer to head of linked list
+ * Return: size
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+
+}
