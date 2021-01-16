31390b61f935e7418da7c7ff9a235af93b137b6f
David Alzate <illker@hotmail.com>
2021-01-16 04:43:56 +0000

    hashish :collision:

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht  == NULL || key == NULL)
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	hashish = ht->array[index];
 
 	if (hashish == NULL)
