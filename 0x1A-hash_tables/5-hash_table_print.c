8c44f712dedab9a2e977452cb35475e0911d040a
David Alzate <illker@hotmail.com>
2021-01-16 03:11:23 +0000

    hashish :collision:

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that prints a hash table
+ * @ht: the hash table
+ * Return: hash value
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *hashish;
+	unsigned long a;
+	int b;
+
+	if (ht == NULL)
+		return;
+
+	for (a = 0; a <= ht->size; a++)
+	{
+		if (ht->array[a])
+		{
+			hashish = ht->array[a];
+			for (b = 0; hashish != NULL; hashish = hashish->next, b++)
+			{
+				if (b == 0)
+					printf(" ");
+				printf("%s': '%s'", hashish->key, hashish->value);
+			}
+		}
+	}
+}
